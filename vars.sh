07ae01c34548117f28c44f1e094b71bf0322dab3
matibaski <matibaski@gmail.com>
2013-08-19 15:11:39 +0200

    Create vars.sh
    the configuration for the hosting-script.

---
+++ vars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+hostingfolders=/var/www    # without slash at the end
+delfolders=/var/deleted-www
+apacheavail=/etc/apache2/sites-available
+apacheenable=/etc/apache2/sites-enabled
+apachedeleted=/etc/apache2/sites-deleted
+mysqlroot="root"
+rootpw="root-pw"
